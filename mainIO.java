d3b0420340fe430cfe4517e3ca4df2d676d9f477
mackenzie-morphew <35542877+mackenzie-morphew@users.noreply.github.com>
2018-01-24 14:06:58 -0700

    Add files via upload
    The initial I/O file with the added bits from today - Jan 24

---
+++ mainIO.java
@@ -0,0 +1,36 @@
+// 
+
+//import necessary files
+import java.io.*;
+import java.util.scanner;
+
+
+public class mainIO {
+	
+	public static void main (String[] args) {
+	
+		BufferedReader fileInput = new BufferedReader(new FileReader(args[0]));
+		BufferedWriter fileOutput = new BufferedWriter(new FileWriter(args[1]));
+		Scanner fileScanner = new Scanner(fileInput);
+		
+		
+	while (fileScanner.haxNext()) {
+		nextLine = fileScanner.nextLine();
+		
+		if (nextLine = "\n"){
+			//do nothing
+		}
+		
+		else-if (nextLine = "forced partial assingnment:") {
+			// forcced partial assingment initialization
+			//read the next line
+			//assign to linked lidst 
+			if (nextLine = "\n") {
+				break;
+			}
+		}
+	}
+		}
+	
+	}
+}
