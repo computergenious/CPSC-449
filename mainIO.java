00f229c7e8a2514b71244c066e02450d158a93a7
mackenzie-morphew <35542877+mackenzie-morphew@users.noreply.github.com>
2018-01-29 13:16:20 -0700

    ver 2.0

--- mainIO.java
+++ mainIO.java
@@ -13,24 +13,47 @@ public class mainIO {
 		BufferedWriter fileOutput = new BufferedWriter(new FileWriter(args[1]));
 		Scanner fileScanner = new Scanner(fileInput);
 		
-		
-	while (fileScanner.haxNext()) {
-		nextLine = fileScanner.nextLine();
-		
-		if (nextLine = "\n"){
-			//do nothing
-		}
-		
-		else-if (nextLine = "forced partial assingnment:") {
-			// forcced partial assingment initialization
-			//read the next line
-			//assign to linked lidst 
-			if (nextLine = "\n") {
-				break;
+		while (fileScanner.haxNext()) {
+			
+			String nextLine = fileScanner.nextLine();
+			
+			if (nextLine == "forced partial assingnment:") {
+				
+				int[] forcedAssignArray;
+				for (int i=0;  i < 8; i++) 
+				{
+					forcedAssignArray[i] = -1;
+				}
+				
+				
+				for ( int i=0; i < 8; i++) 
+				{
+					
+					nextLine = fileScanner.nextLine();
+					
+					if (nextLine == "\n") break;
+					
+					dataLine = nextLine.replaceAll("[(,)]", " ");
+					int mach = fileScanner.nextInt(dataLine);
+					int task = fileScanner.nextInt(dataLine);
+					
+					if (forcedAssignArray[mach] != -1)
+					{
+						//throw exception?? or return some value to indicate failure
+						break;
+					}
+					else 
+					{
+						forcedAssignArray[mach] = task;
+					}
+				}
 			}
+			
+			else if (nextLine == "forbidden machine:")
+			{
+				
+			}
+				
 		}
-	}
-		}
-	
 	}
 }
