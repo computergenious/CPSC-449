94b19fc678c0f1bc13a59d4c3b963b142f7e3aee
ksdhami <32716134+ksdhami@users.noreply.github.com>
2018-01-31 12:00:30 -0700

    Add files via upload
    Currently have arbitrary package names that should be changed

---
+++ BranchAndBound.java
@@ -0,0 +1,67 @@
+package assignment;
+
+public class BranchAndBound {
+
+    public Node branchAndBound(assignment.Node node, assignment.Node best, boolean[][] forbidden, boolean[][] tooNear) throws Exception {
+
+        if (node.getNumAvailableTasks() == 0) {
+            // if all tasks are assigned then break out of recursion
+            best = node;
+        } else {
+            // find first open machine not assigned a task
+            int machine = node.getFirstAvailableMachine();
+
+            // loop through remaining unassigned task and create a branch for each with machine above
+            for(int task : node.getRemainingTasks()) {
+
+                // Check too near tasks hard constraints for the tasks
+                 int prevMach = Node.prevMachIndex(machine);
+                 boolean tooNearPrevFlag = node.getTaskAtIndex(prevMach) != Node.DUMMY_VALUE && tooNear[node.getTaskAtIndex(prevMach)][task];
+
+                 int nextMach = Node.nextMachIndex(machine);
+                 boolean tooNearNextFlag = node.getTaskAtIndex(nextMach) != Node.DUMMY_VALUE && tooNear[task][node.getTaskAtIndex(nextMach)];
+
+                 boolean tooNearFlag = tooNearNextFlag || tooNearPrevFlag;
+
+                 // if too near tasks and forbidden machines constraints met create child node
+                if(!tooNearFlag && !forbidden[machine][task]) {
+                    // copy parent node information and add new task to open machine
+                    Node child = new Node(node, machine, task);
+
+                    // if best node not yet set(not all machines assigned a task
+                    // of if subtree rooted at newly created node has a smaller
+                    // penalty then the current best
+                    //if(best == null || child.getPenaltyPoints() < best.getPenaltyPoints())
+                    if(best == null || child.penalty() < best.penalty()) {
+                        // create subtree of new node recursively
+                        Node nextNode = branchAndBound(child, best, forbidden, tooNear);
+
+                        // if subtree node has a better penalty then the current best node
+                        // assign the best node to be the subtree node
+
+                        //NOTE: different versions shown on how to acheive this
+                        int nextNodePenalty;
+                        if(nextNode == null) { // try moving into next if-statement to create nested
+                            nextNodePenalty = Integer.MAX_VALUE;
+                        } else {
+                            nextNodePenalty = nextNode.penalty();
+                        }
+                        if(best == null || nextNodePenalty < best.penalty()) {
+                            best = nextNode;
+                        }
+
+                        /*if(best == null || nextNode.penalty() < best.penalty()) {
+                            best = nextNode;
+                        }*/
+
+                        /*if(best == null || nextNode.getPenaltyPoints() < best.getPenaltyPoints()) {
+                            best = nextNode;
+                        }*/
+
+                    }
+                }
+            }
+        }
+        return best;
+    }
+}
