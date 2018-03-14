e380b4015f4d92e7db7af0d1eeb815a358745fbd
ksdhami <32716134+ksdhami@users.noreply.github.com>
2018-03-14 16:09:20 -0600

    imported Parser; takes files from command line

--- main.hs
+++ main.hs
@@ -3,27 +3,33 @@ import System.Environment
 import Data.List
 
 import BranchBound
+import Parser
 
 -- for testing purposes arguments to be passed to solver are hard coded using the noChoice.txt input file
 main = do
+    args <- getArgs
+    let readFile = head args
+    handle<- openFile readFile ReadMode
+    contents <- hGetContents handle
+    printer(solver(parsing(lines contents)))
+{-
     let contents = Constraint [[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False]] [[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False]] [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]] [[1,1,1,1,1,1,1,100],[1,1,1,1,1,1,100,1],[1,1,1,1,1,100,1,1],[1,1,1,1,100,1,1,1],[1,1,1,100,1,1,1,1],[1,1,100,1,1,1,1,1],[1,100,1,1,1,1,1,1],[100,1,1,1,1,1,1,1]]
 
     let partials = [(7,0),(6,1),(5,2),(4,3),(3,4),(2,5),(1,6),(0,7)]
     let error = ""
     let solverArguments = (contents, partials, error)
     printer(solver solverArguments)
+-}
 
-
--- change to (args !! 1) once parser is working
 printer :: (Solution,String)->IO()
 printer (solution, []) = do
     args<-getArgs
-    writeFile (args !! 0) ("Solution " ++ unwords(map (\c -> [taskLetter c]) (getAssignment solution))++"; Quality: " ++ show (getPenalty solution)++"\n")
+    writeFile (args !! 1) ("Solution " ++ unwords(map (\c -> [taskLetter c]) (getAssignment solution))++"; Quality: " ++ show (getPenalty solution)++"\n")
 
 
 printer (solution, error) = do
     args<-getArgs
-    writeFile (args !! 0) error
+    writeFile (args !! 1) error
 
 taskLetter 0 = 'A'
 taskLetter 1 = 'B'
