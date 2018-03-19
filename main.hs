033a669a4afa4871bcd24f9ae8551bca15f17454
ksdhami <32716134+ksdhami@users.noreply.github.com>
2018-03-18 21:37:09 -0600

    Update main.hs

--- main.hs
+++ main.hs
@@ -5,21 +5,12 @@ import Data.List
 import BranchBound
 import Parser
 
--- for testing purposes arguments to be passed to solver are hard coded using the noChoice.txt input file
 main = do
     args <- getArgs
     let readFile = head args
     handle<- openFile readFile ReadMode
     contents <- hGetContents handle
     printer(solver(parsing(lines contents)))
-{-
-    let contents = Constraint [[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False]] [[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False]] [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]] [[1,1,1,1,1,1,1,100],[1,1,1,1,1,1,100,1],[1,1,1,1,1,100,1,1],[1,1,1,1,100,1,1,1],[1,1,1,100,1,1,1,1],[1,1,100,1,1,1,1,1],[1,100,1,1,1,1,1,1],[100,1,1,1,1,1,1,1]]
-
-    let partials = [(7,0),(6,1),(5,2),(4,3),(3,4),(2,5),(1,6),(0,7)]
-    let error = ""
-    let solverArguments = (contents, partials, error)
-    printer(solver solverArguments)
--}
 
 printer :: (Solution,String)->IO()
 printer (solution, []) = do
