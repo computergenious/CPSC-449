f6e89dd5d6cfae87ecf40e1b018b904eb14a9fee
Victor Guan <victor.guan@ucalgary.ca>
2018-04-11 01:43:14 -0600

    Checks every section. Only need to add the calculations

--- db6.pl
+++ db6.pl
@@ -12,7 +12,7 @@
 nameTitle("Name:").
 forcedPartialAssignTitle("forced partial assignment:").
 forbiddenTitle("forbidden machine:").
-tooNearTitle("too-near Penalties:").
+tooNearTitle("too-near tasks:").
 machinePenTitle("machine penalties:").
 tooNearPenTitle("too-near penalities").
 
@@ -24,7 +24,7 @@ tooNearPenTitle("too-near penalities").
 %If Error asserts parsing error
 compare([],[]):- !, write('Compare Succesful'),nl.
 compare([H1|T1], [H2|T2]):-
-	write('compare: '), write(H1),write(' to '), write(H2), nl,
+	%write('compare: '), write(H1),write(' to '), write(H2), nl,
 	H1 == H2 
 		-> compare(T1, T2) 
 		; asserta(error('Error while parsing input file')), write('FAIL').
@@ -32,31 +32,16 @@ compare([H1|T1], [H2|T2]):-
 	
 
 
-%Beginning
+%---Beginning---
 %Opens file ready to be read
 read_from_file(File) :-					
 	open(File, read, Stream),
 	read_name(Stream),
 	close(Stream).
-	
-%Stuff for readLine
-read_line(Stream, CharList) :-
-	read_char(Stream, CharList),
-	compare(CharList, nameTitle).
-	
-read_char(Stream, CharList):-
-	get_char(Stream, Char),
-	process_char(Stream,Char, CharList).
-	
-process_char(_,\n,_):- write('end of line').      %stops progression on this line
-process_char(Stream,X,CharList) :- 
-	append(CharList, X, CharList),
-	read_char(Stream, CharList).
-
 
 
 	
-%Reads "Name:"
+%---Reads "Name:"---
 read_name(Stream):-
 	write('----- Name: -----'), nl,
 	get_code(Stream, Char1),% put_char(Char1),
@@ -65,8 +50,10 @@ read_name(Stream):-
 	get_code(Stream, Char4),% put_char(Char4),
 	get_code(Stream, Char5),% put_char(Char5),
 	List = [Char1, Char2, Char3, Char4, Char5],
+	write('Name List:    '), write(List),nl,
 	%nameTitle([X,Y,Z,W,V]),
 	nameTitle(X),
+	write('Expect List:  '), write(X), nl,
 	compare(List, X),
 	get_code(Stream, Charnl),
 	read_name_2(Stream).
@@ -102,7 +89,7 @@ skip_line_forced(Stream, Char) :-		%If Char is something, send to forced
 
 
 
-%Reads "forced partial assignment"
+%---Reads "forced partial assignment"---
 read_forced(Stream, Char1):-
 	write('----- forced partial assignment -----'), nl,
 	get_code(Stream, Char2),get_code(Stream, Char3),get_code(Stream, Char4),
@@ -116,14 +103,15 @@ read_forced(Stream, Char1):-
 			Char7, Char8, Char9, Char10, Char11, Char12, 
 			Char13, Char14, Char15, Char16, Char17, Char18, 
 			Char19, Char20, Char21, Char22, Char23, Char24, Char25, Char26],
-	write('forced List: '), write(List), nl,
+	write('forced List:  '), write(List), nl,
 	forcedPartialAssignTitle(X),
+	write('Expect List:  '), write(X), nl,
 	compare(List, X),
-	get_code(Stream, Charnl).				%Gets rid of following \n
 	read_forced_math(Stream).
 	
-%FORCED PARTIAL STUFF HERE
+%ADD FORCED PARTIAL STUFF HERE----------------------------------------
 read_forced_math(Stream) :-
+	get_code(Stream, Char),
 	%stuff
 	%Should stop when \n\n is found
 	write('End of Forced'), nl, nl,
@@ -145,7 +133,7 @@ skip_line_forbidden(Stream, Char) :-		%If Char is something, send to forced
 
 
 
-%Reads "forced partial assignment"
+%---Reads "forbidden partial assignment"---
 read_forbidden(Stream, Char1):-
 	write('----- forbidden machine: -----'), nl,
 	get_code(Stream, Char2),get_code(Stream, Char3),get_code(Stream, Char4),
@@ -156,12 +144,128 @@ read_forbidden(Stream, Char1):-
 	List = [Char1, Char2, Char3, Char4, Char5, Char6, Char7, Char8, Char9, Char10, Char11, Char12, Char13, Char14, Char15, Char16, Char17, Char18],
 	write('forbidden List: '), write(List), nl,
 	forbiddenTitle(X),
+	write('Expect List:    '), write(X), nl,
 	compare(List, X),
-	get_code(Stream, Charnl).
+	read_forbidden_math(Stream).
 
+%ADD STUFF HERE ----------------------------------------
+read_forbidden_math(Stream) :-
+	get_code(Stream, Char),
+	%stuff
+	%Should stop when \n\n is found
+	write('End of Forbidden'), nl, nl,
+	skip_line_tooNear(Stream).			%Continues to the next section
+
+
+
+%Skips \n before TOONEAR
+skip_line_tooNear(Stream) :-				%Function to skip all \n before forced
+	get_code(Stream, Char),
+	skip_line_tooNear(Stream, Char).
+
+skip_line_tooNear(Stream, 10):-			%If Char is \n(10), continue skipping
+	skip_line_tooNear(Stream).
+skip_line_tooNear(Stream, 32):-			%If Char is ' '(32), continue skipping
+	skip_line_tooNear(Stream).
+skip_line_tooNear(Stream, Char) :-		%If Char is something, send to forced
+	read_tooNear(Stream, Char).	
+
+
+%----Read tooNear----
+read_tooNear(Stream, Char1) :-
+	write('----- too-near task: -----'), nl,
+	get_code(Stream, Char2),get_code(Stream, Char3),get_code(Stream, Char4),
+	get_code(Stream, Char5),get_code(Stream, Char6),get_code(Stream, Char7),get_code(Stream, Char8),
+	get_code(Stream, Char9),get_code(Stream, Char10),get_code(Stream, Char11),get_code(Stream, Char12),
+	get_code(Stream, Char13),get_code(Stream, Char14),get_code(Stream, Char15),
+	List = [Char1, Char2, Char3, Char4, Char5, Char6, Char7, Char8, Char9, Char10, Char11, Char12, Char13, Char14, Char15],
+	write('tooNear List: '), write(List), nl,
+	tooNearTitle(X),
+	write('Expect List:  '), write(X), nl,
+	compare(List, X),
+	read_tooNear_math(Stream).
+
+%ADDDDD STUFF HERE----------------------------------------
+read_tooNear_math(Stream):-
+	get_code(Stream, Char),
+	%stuff
+	%Should stop when \n\n is found
+	write('End of Too Near'), nl, nl,
+	skip_line_machPen(Stream).			%Continues to the next section
 
 
 	
-append_char(Char, List):-
-	append(List, Char, List2),
-	List = List2.
+%Skips \n before machinePen
+skip_line_machPen(Stream) :-				%Function to skip all \n before forced
+	get_code(Stream, Char),
+	skip_line_machPen(Stream, Char).
+
+skip_line_machPen(Stream, 10):-			%If Char is \n(10), continue skipping
+	skip_line_machPen(Stream).
+skip_line_machPen(Stream, 32):-			%If Char is ' '(32), continue skipping
+	skip_line_machPen(Stream).
+skip_line_machPen(Stream, Char) :-		%If Char is something, send to forced
+	read_machPen(Stream, Char).	
+
+%----Read machPen----
+read_machPen(Stream, Char1) :-
+	write('----- Machine Pen: -----'), nl,
+	get_code(Stream, Char2),get_code(Stream, Char3),get_code(Stream, Char4),
+	get_code(Stream, Char5),get_code(Stream, Char6),get_code(Stream, Char7),get_code(Stream, Char8),
+	get_code(Stream, Char9),get_code(Stream, Char10),get_code(Stream, Char11),get_code(Stream, Char12),
+	get_code(Stream, Char13),get_code(Stream, Char14),get_code(Stream, Char15),get_code(Stream, Char16),
+	get_code(Stream, Char17),get_code(Stream, Char18),
+	List = [Char1, Char2, Char3, Char4, Char5, Char6, Char7, Char8, Char9, Char10, Char11, Char12, Char13, Char14, Char15, Char16, Char17, Char18],
+	write('machine List: '), write(List), nl,
+	machinePenTitle(X),
+	write('Expect List:  '), write(X), nl,
+	compare(List, X).
+	read_machPen_math(Stream).
+	
+%AADDD STUFF HERE----------------------------------------
+read_machPen_math(Stream):-
+	get_code(Stream, Char),
+	%stuff
+	%Should stop when \n\n is found
+	write('End of Machine Pen'), nl, nl,
+	skip_line_tooNearPen(Stream).			%Continues to the next section
+
+
+	
+%Skips \n before tooNearPen
+skip_line_tooNearPen(Stream) :-				%Function to skip all \n before forced
+	get_code(Stream, Char),
+	skip_line_tooNearPen(Stream, Char).
+
+skip_line_tooNearPen(Stream, 10):-			%If Char is \n(10), continue skipping
+	skip_line_tooNearPen(Stream).
+skip_line_tooNearPen(Stream, 32):-			%If Char is ' '(32), continue skipping
+	skip_line_tooNearPen(Stream).
+skip_line_tooNearPen(Stream, Char) :-		%If Char is something, send to forced
+	read_tooNearPen(Stream, Char).	
+	
+	
+%----Read TooNearPen----
+read_tooNearPen(Stream, Char1) :-
+	write('----- Too Near Pen: -----'), nl,
+	get_code(Stream, Char2),get_code(Stream, Char3),get_code(Stream, Char4),
+	get_code(Stream, Char5),get_code(Stream, Char6),get_code(Stream, Char7),get_code(Stream, Char8),
+	get_code(Stream, Char9),get_code(Stream, Char10),get_code(Stream, Char11),get_code(Stream, Char12),
+	get_code(Stream, Char13),get_code(Stream, Char14),get_code(Stream, Char15),get_code(Stream, Char16),
+	get_code(Stream, Char17),get_code(Stream, Char18),get_code(Stream, Char19),
+	List = [Char1, Char2, Char3, Char4, Char5, Char6, 
+			Char7, Char8, Char9, Char10, Char11, Char12, 
+			Char13, Char14, Char15, Char16, Char17, Char18, 
+			Char19],
+	write('tooNearPen List: '), write(List), nl,
+	tooNearPenTitle(X),
+	write('Expect List:     '), write(X), nl,
+	compare(List, X).
+	read_tooNearPen_math(Stream).
+	
+%ADD STUFF HERE ----------------------------------------
+read_tooNearPen_math(Stream):-
+	get_code(Stream, Char),
+	%stuff
+	%Should stop when \n\n is found
+	write('End of tooNearPen'), nl, nl.
